ee33febc1efbcb2fb96eb852601094606209a35f
aristotleboi <gage24paul@gmail.com>
2022-01-07 16:40:57 -0800

    have play round and computer selection working with player prompt. score is now counting up exclusivly for player

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,4 @@
+//funtion to randomly have the CPU select rock,paper, or scissors
 function computerPlay() {
     let selection;
     let computerNumber = Math.floor(Math.random()*3) + 1;
@@ -11,33 +12,69 @@ function computerPlay() {
     return selection;
 }
 
-let computerSelection = computerPlay();
-let playerSelection = 'rock'
-console.log(computerSelection);
 
+let computerSelection;
+let playerSelection;
+let roundMessage;
+let playerScore = 0;
+let computerScore = 0;
+
+
+//playRound runs through one round of rock paper scissors and returns a message of win loss or draw
 function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay();
+    console.log(computerSelection);
     let message;
-    if (playerSelection == 'rock' && computerSelection == 'scissors') {
+    playerSelection = prompt("Rock, Paper, Scissors");
+    //9 comparisons for each outcome of rock paper scissors
+    if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') {
         message = 'You Win! ' + playerSelection + ' beats ' + computerSelection;
-    } else if (playerSelection == 'rock' && computerSelection == 'rock') {
-        message = 'Draw'
-    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        message = 'You Lose! ' + computerSelection + ' beats ' + playerSelection;
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        message = 'You Win! ' + playerSelection + ' beats ' + computerSelection;
-    } else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
+    } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'rock') {
         message = 'Draw';
-    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+    } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper') {
         message = 'You Lose! ' + computerSelection + ' beats ' + playerSelection;
-    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper') {
         message = 'You Win! ' + playerSelection + ' beats ' + computerSelection;
-    } else if (playerSelection == 'paper' && computerSelection == 'paper') {
+    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'scissors') {
         message = 'Draw';
-    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'rock') {
+        message = 'You Lose! ' + computerSelection + ' beats ' + playerSelection;
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock') {
+        message = 'You Win! ' + playerSelection + ' beats ' + computerSelection;
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'paper') {
+        message = 'Draw';
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'scissors') {
         message = 'You Lose! ' + computerSelection + ' beats ' + playerSelection;
     }
-
-    return message
+    return message;
 }
 
-console.log(playRound(playerSelection, computerSelection));
+// a function to keep score over 5 rounds
+function game() {
+    let win = "Win";
+    let lose = "Lose";
+    let roundMessage = playRound();
+    console.log(roundMessage);
+    if (roundMessage.indexOf(win)) {
+        console.log(roundMessage);
+        console.log("Your Score " + playerScore + "  CPU score " + computerScore);
+        return playerScore = playerScore + 1;
+    } else if (roundMessage.indexOf(lose)) {
+        console.log(roundMessage);
+        console.log("Your Score " + playerScore + "  CPU score " + computerScore);
+        return computerScore = computerScore + 1;
+    } else {
+        console.log("Draw");
+        console.log("Your Score " + playerScore + "  CPU score " + computerScore);
+        return "Draw"
+    } 
+    
+    
+
+}
+
+console.log(game());
+console.log(game());
+console.log(game());
+console.log(game());
+console.log(game());
